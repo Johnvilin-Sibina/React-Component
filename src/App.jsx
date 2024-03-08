adfc8c0782dfbc6662c1e66fd97b735c886f4a66
Johnvilin-Sibina <johnvilinsibina@gmail.com>
2024-03-08 18:34:52 +0530

    Added Examples.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,25 @@
 import Card from "./Card"
+//Basic example to create a component and exporting and importing it 
+/*function App(){
+  return (
+    <div><Card/></div>
+  )
+}
+export default App*/
+
+//Example for reusable components
+/*function App(){
+  return (
+    <div>
+      <Card place="World"/>
+      <Card place="Earth"/>
+      <Card place="Mars"/>
+    </div>
+  )
+}
+export default App*/
+
+//Example of reusable component using loop
 function App() {
   let places = [
     {
